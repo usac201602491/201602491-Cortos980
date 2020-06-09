6f0a8a9f74d5609460da8c30b88344d07f0d53f7
usac201602491 <2931416571705@ingenieria.usac.edu.gt>
2020-06-09 12:18:36 -0600

    Corto1 v2

---
+++ corto1/corto1.py
@@ -0,0 +1,30 @@
+N = 5
+lista = []
+
+#archivo = open('collatz.txt','w')
+
+for o in range(2,N):
+    num = o
+
+    def esPar(num):
+        if num%2>0:
+            return False
+        else:
+            return True
+
+    while num!=1:
+        if(esPar(num)):
+            lista.append(num)
+            num=num/2          
+        else:
+            lista.append(num)
+            num = int(3*num+1)
+
+    if(num==1):
+        lista.append(num)
+
+    for i in lista:
+       # archivo.write(str(i))
+        print(i)
+#archivo.close()
+

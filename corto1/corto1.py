b5ccdb48056d6386893d2d4322a875aed13f3962
usac201602491 <2931416571705@ingenieria.usac.edu.gt>
2020-06-09 12:40:52 -0600

    Corto1 v6

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,18 +1,18 @@
-N = 491
+N = 491 # Limite de iteraciones
 lista = []
 
 archivo = open('collatz.txt','w')
 
-for o in range(2,N+1):
+for o in range(2,N+1): # Se recorre hasta el limite
     num = o
 
-    def esPar(num):
+    def esPar(num): # Se define si es par
         if num%2>0:
             return False
         else:
             return True
 
-    while num!=1:
+    while num!=1: # Si es par num/2 si es impar 3num+1
         if(esPar(num)):
             lista.append(int(num))
             num=int(num/2)
@@ -23,8 +23,8 @@ for o in range(2,N+1):
     if(num==1):
         lista.append(int(num))        
     print(lista)
-    archivo.write(str(lista))
-    archivo.write(str("\n"))
+    archivo.write(str(lista)) # Agrega al archiva la lista, luego la vacia para limpiar
+    archivo.write(str("\n"))  # los valores y volver a ingresar los nuevos
     archivo.write(str("\n"))
     archivo.write(str("\n"))
     lista.clear()    
